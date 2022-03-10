91b4a1f9c5cbdd816c60a9e54904dd8f5053c7ba
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-10 21:31:27 +0530

    uc1 : employee is present or not

--- Empcheck attendance/Empcheck attendance/Program.cs
+++ Empcheck attendance/Empcheck attendance/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Employee Wage");
+
+Random random = new Random();
+const int present = 1,
+          absent = 0;
+
+int isPresent = random.Next(2);
+
+if (isPresent == present)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is Absent");
+}
