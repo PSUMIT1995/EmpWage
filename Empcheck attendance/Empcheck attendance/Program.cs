ae316c48924a02206dca530f9cc9d6d3cc22c6ed
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-11 01:14:06 +0530

    Uc3 : parttime

--- Empcheck attendance/Empcheck attendance/Program.cs
+++ Empcheck attendance/Empcheck attendance/Program.cs
@@ -1,14 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage");
 
-
-int IS_FULL_TIME = 1;
+int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
 int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
+if (empCheck == IS_PART_TIME)
+{
+    empHrs = 4;
+}
+else if (empCheck == IS_FULL_TIME)
 {
     empHrs = 8;
 }
