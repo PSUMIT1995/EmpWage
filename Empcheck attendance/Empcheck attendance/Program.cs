534b6fbe3d7cf2f68f840735bdd1674c3968368b
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-11 00:49:04 +0530

    uc2 : employee wage

--- Empcheck attendance/Empcheck attendance/Program.cs
+++ Empcheck attendance/Empcheck attendance/Program.cs
@@ -1,17 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage");
 
+
+int IS_FULL_TIME = 1;
+int EMP_RATE_PER_HOUR = 20;
+int empHrs = 0;
+int empWage = 0;
 Random random = new Random();
-const int present = 1,
-          absent = 0;
-
-int isPresent = random.Next(2);
-
-if (isPresent == present)
+int empCheck = random.Next(0, 2);
+if (empCheck == IS_FULL_TIME)
 {
-    Console.WriteLine("Employee is Present");
+    empHrs = 8;
 }
 else
 {
-    Console.WriteLine("Employee is Absent");
+    empHrs = 0;
 }
+empWage = empHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Emp Wage : " + empWage);
