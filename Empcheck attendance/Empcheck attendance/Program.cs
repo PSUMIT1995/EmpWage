6e9a042499013326f22dd3882705814ff37ca36e
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-11 23:56:50 +0530

    Uc6 : total working hrs reach

--- Empcheck attendance/Empcheck attendance/Program.cs
+++ Empcheck attendance/Empcheck attendance/Program.cs
@@ -2,24 +2,36 @@
 Console.WriteLine("Welcome to Employee Wage");
 int IS_PART_TIME = 0;
 int IS_FULL_TIME = 1;
-int IS_Absent = 2;
+int IS_ABSENT = 2;
+int NUM_OF_WORKING_DAYS = 20;
 int EMP_RATE_PER_HOUR = 20;
+int MAX_HRS_IN_MONTH = 100;
 
 int empHrs = 0;
-int empWage = 0;
-Random random = new Random();
-int empCheck = random.Next(0, 3);
-switch (empCheck)
+int totalEmpHrs = 0;
+int totalWorkingDays = 0;
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
-    case 0: IS_PART_TIME: Console.WriteLine("Employee is Present part Time");
-        empHrs = 4;
-        break;
-    case 1: IS_FULL_TIME: Console.WriteLine("Employee is Present full Time");
-        empHrs = 8;
-        break;
-    default : Console.WriteLine("Employee is Absent");
-        empHrs = 0;
-        break;
+    totalWorkingDays++;
+    Random random = new Random();
+    int empCheck = random.Next(0, 3);
+    switch (empCheck)
+    {
+        case 0:
+        IS_PART_TIME: Console.WriteLine("Employee is Present part Time");
+            empHrs = 4;
+            break;
+        case 1:
+        IS_FULL_TIME: Console.WriteLine("Employee is Present full Time");
+            empHrs = 8;
+            break;
+        default:
+            Console.WriteLine("Employee is Absent");
+            empHrs = 0;
+            break;
+    }
+    totalEmpHrs += empHrs;
+    Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
 }
-empWage = empHrs * EMP_RATE_PER_HOUR;
-Console.WriteLine("Emp Wage : " + empWage);
+int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+Console.WriteLine("Total Emp Wage : " + totalEmpWage);
